f44d9a4d1088e4926707baea35781eb9b9b6a73a
Alex Frost <zufrost.a@gmail.com>
2020-04-17 15:32:05 +0300

    Hello world app

---
+++ src/com/zufrost/learn/Main.java
@@ -0,0 +1,9 @@
+package com.zufrost.learn;
+
+public class Main {
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
